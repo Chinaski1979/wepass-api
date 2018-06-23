7f28c612a104b8ac644138d0a2105cecbe57b06c
MarvinAmador7 <marvin.ac.cr@gmail.com>
2018-06-22 21:34:32 -0600

    [#158574709] remove sudo code

--- server/helpers/email/index.js
+++ server/helpers/email/index.js
@@ -8,20 +8,9 @@ export default function buildEmailTemplate (type, data) {
       const html = await emailData(type, data);
       const mailgun = new Mailgun({ apiKey : process.env.MG_API_KEY, domain : process.env.MG_DOMAIN });
       const emailConfig = Object.assign(data, { html : html.template });
-      // console.log(emailConfig);
-      // const email = await mailgun.messages().send(emailConfig, (err, body) => {
-      //   console.log('mailgun response', mailgun, body, err);
-      // });
-
-      mailgun.messages().send(emailConfig, (err, body) => {
-        if (err) {
-          console.log('got an error: ', err);
-        }
-        console.log(body);
-        resolve('body', body);
-      });
+      const email = await mailgun.messages().send(emailConfig);
+      resolve(email);
     } catch (err) {
-      console.log('error', err);
       reject(err);
     }
   });

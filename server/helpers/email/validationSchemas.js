a457ddd65e29da3af15a15debbee7ef4b5fc64f7
MarvinAmador7 <marvin.ac.cr@gmail.com>
2018-06-24 15:50:02 -0600

    [#158574709] fix email validation

--- server/helpers/email/validationSchemas.js
+++ server/helpers/email/validationSchemas.js
@@ -6,6 +6,9 @@ const baseEmail = Joi.object().keys({
   from    : Joi.string().required(),
   to      : Joi.string().required(),
   subject : Joi.string().required(),
+  title   : Joi.string().optional(),
+  content : Joi.string().optional(),
+  footer  : Joi.string().optional(),
 });
 
 export function emailData (data) {
